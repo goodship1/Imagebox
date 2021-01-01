ee93a1e59fe436ea715f7517ad67b7ee5c088ad0
Gavin Goodship <msgoodship1@gmail.com>
2021-01-01 23:41:07 +0000

    Create Patch.py

---
+++ Attacks/Patch.py
@@ -0,0 +1,11 @@
+class Patch(object):
+
+    def __str__(self):
+        return "Advarsarial patch"
+        
+    
+    def generatepath(self):
+        pass
+        
+    def applypatch(image):
+        pass
