06e823369f00319cb7413132647978f5d903c3a0
Gavin Goodship <msgoodship1@gmail.com>
2021-01-02 22:48:01 +0000

    Update Constrast.py

--- Attacks/Constrast.py
+++ Attacks/Constrast.py
@@ -1,10 +1,11 @@
 import cv2 as cv
 class Contrast(object):
+    '''Class for adding constrast to image to reduce to image quality'''
     
     def __str__(self):
         return "functions for adding constrast"
 
-    def constrast(image,leveofconstrast):
+    def constrast(self,image,levelofconstrast):
         alpha = constrast
         beta = 0
         convertimage = cv.imread(image)
