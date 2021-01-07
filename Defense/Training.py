52b7ecbed3ab9abf2e2f8763a57dd3a547caff33
goodship1 <msgoodship1@gmail.com>
2021-01-07 01:17:02 +0000

    adding to training

--- Defense/Training.py
+++ Defense/Training.py
@@ -1,4 +1,4 @@
-from attacks import noise
+from attacks import noise,constrast
 
 class Training(object):
     
@@ -7,9 +7,13 @@ class Training(object):
     
     def __init__(self):
         self.noise = Noise()
+        self.contrast = Contrast()
     
    def _gussianfile(image,filepath):
        '''adds gussian noise images to file path'''
+       pass
+
+   def _saltpepperfile(image,filepath):
        pass
    def gussianTraining(image,filePath,numberofsamples):
        '''function for generating guassian noise
@@ -17,6 +21,9 @@ class Training(object):
         for x in range(numberofsamples):
              gussian_image = self.noise.Gussian(image)
   
+  def constrasttraining(image,filepath,numberofsamples):
+      pass
+
   def saltpeppertraining(image,filepath,numberofsamples):
       speckle_noise =  self.noise.Speckle(image)
 
