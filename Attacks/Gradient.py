618639c651ab0c1c0e88927e80ecf166046cb549
goodship1 <msgoodship1@gmail.com>
2021-01-24 00:45:04 +0000

    adding more attacks

--- Attacks/Gradient.py
+++ Attacks/Gradient.py
@@ -1,10 +1,14 @@
+import tensorflow as tf
 class Gradient(object):
     '''Gradient class for Gradient based attacks on
     images such as FGSM'''
+    def __init__(self):
+        self.loss  = None
+        self.tape = None
 
     def __str__(self):
         return "Gradient attacks on images"
 
-    def FGSM(self,image):
+    def FGSM(self,image,label):
         '''FGSM attack'''
         pass
---
+++ Attacks/Scale.py
@@ -0,0 +1,11 @@
+class Scale(object):
+    '''Downscale image attacks on image neural netorks'''
+
+    def __str__(self):
+        return "Down scaling image attack"
+
+
+    def downscale(self,imageone,imagetwo,targetshape):
+        '''down scaling image attacks'''
+        pass
+
