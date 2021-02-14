fb3d34a2c66de1d627a18ecdaaa25097241a5c8c
goodship1 <msgoodship1@gmail.com>
2021-02-14 01:18:23 +0000

    grad

--- Attacks/Gradient.py
+++ Attacks/Gradient.py
@@ -1,14 +1,28 @@
+import error as e
 import tensorflow as tf
-class Gradient(object):
-    '''Gradient class for Gradient based attacks on
-    images such as FGSM'''
-    def __init__(self):
-        self.loss  = None
-        self.tape = None
+class Gradiant(object):
+  """FGSM Attack implemention"""
 
-    def __str__(self):
-        return "Gradient attacks on images"
+  def __init__(self):
+    self.tape = tf.GradientTape()
+    self.loss = tf.keras.losses.CategoricalCrossentropy()
+    self.error =  Error()
+  
+  def __str__(self):
+    return "Gradient class for the implementation of the gradiant attacks"
+  
+  def errorhandling(self):
+    return self.error.FGSMerror()
 
-    def FGSM(self,image,label):
-        '''FGSM attack'''
-        pass
+  def FGSM(self,image,label,model):
+    """FGSM attack on images"""
+    try:
+      self.tape.watch(image)#Watches the gradient of input images
+      pred = model.predict(image)#Makes the neural network prediction
+      loss = self.loss(label,image)#calualtes the loss 
+      gradiant = self.tape.gradient(loss, input_image)#calcualtes the gradiant of the image
+      signed =  self.tape.sign(gradiant)#calculates the gradaint of image
+      return signed
+    except:
+      print(self.errorhandling())
+
