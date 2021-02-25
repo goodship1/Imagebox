cd77f15060db252b58f23137d002065e2e177095
goodship1 <msgoodship1@gmail.com>
2021-02-25 00:13:46 +0000

    adding to patch.py

--- Attacks/Patch.py
+++ Attacks/Patch.py
@@ -1,13 +1,14 @@
 from PIL import Image
 import random
+import cv2 as cv
 class Patch(object):
     '''Class for generating advarsarial patch attack'''
 
     def __str__(self):
-        return "Advarsarial patch"
+        return "Advarsarial patch's"
     
    
-     def generatepatch(self,image ,rbg = (0,0,0)):
+    def generatepatch(self,image ,rbg = (0,0,0)):
         '''Helper function to generate black patch
         image -> user image
         return -> np.array
@@ -25,15 +26,53 @@ class Patch(object):
         '''Modfied version of sample based'''
         patch = self.generatepatch(image,rbg)
         image = Image.open(image)
-        width = image.shape[0]
-        height = image.shape[1]
+        width = image.size[0]
+        height = image.size[1]
         random_width = random.randint(1,width)-1
         random_height = random.randint(1,height)-1
         image.paste(patch,(random_width,random_height))
         return (image,np.array(image))
   
 
-    
+    def generatesample(self,image,k):
+        '''generate image samples
+        image -> user inputted image'''
+        image = cv.imread(image)
+        width = image.shape[0]
+        height = image.shape[1]
+        sample_set = list()
+        for x in range(width):
+            for y in range(height):
+                    sample_set.append((x,y))
+        
+        random.shuffle(sample_set)
+        index = 0#starting index
+        res = [0]*k
+        sample_len = len(sample_set)
+        for index in range(k):
+            res[index] = sample_set[index]
+        
+        while index < sample_len:
+            random_pick = random.randrange(index+1)
+            if random_pick < k:
+                res[random_pick] = sample_set[index]
+            index+=1
+        return res
+
+    def HPA(self,image,model=None,classfaction =None,label = None,samples =3):
+      esp =  self.generatesample(image,samples)
+      patch = self.generatepatch(image)
+      image = Image.open(image)
+      for e in esp:
+        image.paste(patch,(e))
+      attack_info = (image,np.array(image),esp)
+      if model == None:
+        return (image,np.array(image),esp)
+      if model != None and classfaction == "binary":
+        im =  np.array(image)
+        return np.argmax(model.predict(im), axis=-1)
+      if model !=None and classfaction == "Multi":
+        return (model.predict(im) > 0.5).astype("int32")
 
     def samplebased(self,image):
         '''Sample based patch attack gray scale
@@ -48,82 +87,5 @@ class Patch(object):
         random_height = random.randint(1,height)
         image.paste(patch,(random_width,random_height))
         return (image,np.array(image))
-            
-    def generatesample(self,image):
-        '''generate image samples
-        image -> user inputted image'''
-        image = cv.imread(image)
-        width = image.shape[0]
-        height = image.shape[1]
-        sample_set = list()
-        for x in range(width):
-            for y in range(height):
-                    sample_set.append((x,y))
+      
         
-        random.shuffle(sample_set)
-        k = 3#number of samples generated
-        index = 0#starting index
-        res = [0]*k
-        sample_len = len(sample_set)
-        for index in range(k):
-            res[index] = sample_set[index]
-        
-        while index < sample_len:
-            random_pick = random.randrange(index+1)
-            if random_pick < k:
-                res[random_pick] = sample_set[index]
-            index+=1
-        return res
-
-
-    def generatenoisepatch(self,image):
-        pass
-
-    def noisepatch(self,image):
-        pass
-
-
-    def loadtextures(self):
-        pass
-
-    def HPA(self,image,model,label = None):
-        epsilion = self.generatesample(image)
-        patch = self.generatepatch(Iimage)
-        adv = list()
-        image = Image.open(image)
-        for x in eplison:
-            place = image.paste(patch,(x))
-            convert =  np.array(place)
-            adv.append(convert)
-        if label != None:
-            for test in adv:
-                pred=model.predict(test)
-                if pred == label:
-                    return 1
-        return np.array(adv)
-
-
-
-
-
-        
-    def MPARBG(self,image):
-        pass
-    
-    def TPA(self,image):
-        pass
-
-    def texture(self,image):
-        pass
-    
-    def generatepatch(self,model):
-        '''Generating advarsarial patch on image'''
-        pass
-    
-    def locationofpatch(self,location):
-        '''Location of patch to be applied'''
-        pass
-        
-    def applypatch(self,image):
-        '''applying the advarsarial patch of choosen image'''
-        pass
