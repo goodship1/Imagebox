92af68675de4b8c114746af908fae6345cd25cb0
goodship1 <msgoodship1@gmail.com>
2021-03-03 20:45:56 +0000

    updating noise.py to include models and labels

--- Attacks/Noise.py
+++ Attacks/Noise.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2 as cv
+import keras
 
 class Noise(object):
     '''Class for adding different types of
@@ -9,53 +10,90 @@ class Noise(object):
     def __str__(self):
         return "A class for adding noise to images"
 
-    def guassian(self,image):
+    def guassian(self,image,model=None,label=None):
 	       image = cv.imread(image)
 	       guassian  = np.random.normal(0,mu,image.size)
 	       guassian = gussian.reshape(image.shape[0],image.shape[1],image.shape[2]).astype('uint8')
 	       noise_image = cv.add(image,guassian)
-	       return noise_image
+	       if model != None and label !=None:
+			   pred = np.argmax(model.predict(noise_image), axis=-1)
+			   return pred
+	       elif model != None:
+			   return noise_image
 	       
 		
 
-    def gammanoise(self,image):
+    def gammanoise(self,image,model=None,label=None):
 			image = cv.imread(image)
 			gamma = np.random.gamma(100,1,image.size)
 			gamma =  gamma.reshape(image.shape[0],image.shape[1],image.shape[2]).astype('uint8')
 			noise_image =  cv.add(image,gamma)
-			return noise_image
+			if model != None and label !=None:
+				pred = np.argmax(model.predict(noise_image), axis=-1)
+				return pred
+			elif model == None:
+				return noise_image
 
 
-    def rayleighnoise(self,image):
+    def rayleighnoise(self,image,model = None,label=None):
         	 image =  cv.imread(image)
         	 rayleigh =  np.random.rayleigh(100,image.size)
         	 rayleigh =  rayleigh.reshape(image.shape[0],image.shape[1],image.shape[2]).astype('uint8')
         	 noise  = cv.add(image,rayleigh)
-        	 return noise
+        	 if model != None and label != None:
+				 pred = np.argmax(model.predict(noise), axis=-1)
+				 return pred
+        	 elif model == None:
+				 return noise
     
-    def expeontialnoise(self,image):
+    def expeontialnoise(self,image,model=None,label=None):
 			image = cv.imread(image)
 			exp = np.random.exponential(1,image.size)
 			exp = exp.reshape(image.shape[0],image.shape[1],image.shape[2]).astype('uint8')
 			noise =  cv.add(image,exp)
-			return noise
+			if model !=None and label!=None:
+				pred = np.argmax(model.predict(average_filter), axis=-1)
+				return pred
+			else:
+				return noise
     
-    def speckle(self,image):
+    def speckle(self,image,model=None,label=None):
         
-        	image = cv.open(image)
+        	image = cv.imread(image)
         	speckle = np.random(0,1,image.size)
        		speckle = speckle.reshape(image.shape[0],image.shape[1],image.shape[2]).astype('uint8')
         	image =  image + image
         	noise_image  = image * speckle
-        	return noise_image
+        	if model != None and label != None:
+				pred = np.argmax(model.predict(noise_image), axis=-1)
+				return pred
+		elif model != None:
+				return noise_image
     
-    def possionnoise(self,image):
-			image =  cv.open(image)
+    def possionnoise(self,image,model =None,label=None):
+			image =  cv.imread(image)
 			pos = np.random.poisson(10,image.size)
 			pos = pos.reshape(image.shape[0],image.shape[1],image.shape[2]).astype('uint8')
 			noise =  cv.add(image,pos)
-			return noise
-		
+			if model != None and label!= None:
+				pred = np.argmax(model.predict(noise), axis=-1)
+				return pred
+			elif model == None:
+				return noise
+    
+    def browiannoise(self,image,scale,mean = 1,model=None,label=None):
+                 
+                 image = cv.imread(image)
+                 browian = np.random.wald(mean,scale,image.size)
+                 browian = np.random.reshape(image.shape[0],image.shape[1],image.shape[2]).astype('uint8')
+                 noise = cv.add(image,browian)
+                 if model!=None and label !=None:
+					 noise = np.array(noise)
+					 pred = np.argmax(model.predict(noise), axis=-1)
+					 return pred
+                 elif model == None:
+					 return noise
+			     
 	      	
 
    
