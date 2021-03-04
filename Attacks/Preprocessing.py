7552fa2cb7c2f7eddf096b2f5d9815f428c3b580
goodship1 <msgoodship1@gmail.com>
2021-03-04 22:27:40 +0000

    preprocessing.py

---
+++ Attacks/Preprocessing.py
@@ -0,0 +1,11 @@
+class Preprocessing(object):
+    '''Downscale image attacks on image neural netorks'''
+
+    def __str__(self):
+        return "Down scaling image attack"
+
+
+    def downscale(self,imageone,imagetwo,targetshape):
+        '''down scaling image attacks'''
+        pass
+
