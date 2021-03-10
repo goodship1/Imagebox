1b5f9ccd8cd03f428c3eb828b30985d30cdd3f22
goodship1 <msgoodship1@gmail.com>
2021-03-10 15:45:38 +0000

    removal gradient.pyc

--- Attacks/Gradcam.py
+++ Attacks/Gradcam.py
@@ -1,9 +1,17 @@
 import keras
 import numpy as np
+import tensorflow as tf
 
 class Gradcam(object):
-
-    def __init__(self,model,layername):
+    
+    def __init__(self,model,layername=None,index):
         self.model = model
         self.layername = layername
+        self.classindex =  index
+    
+    def __str__(self):
+        return "Grad cam implementation"
+    
 
+    def checklayername(self):
+        '''
