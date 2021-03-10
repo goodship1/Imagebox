1bbce188ad94655474b2a3c3cd553e828d585ca9
goodship1 <msgoodship1@gmail.com>
2021-03-10 15:53:57 +0000

    adding grad cam

--- Attacks/Gradcam.py
+++ Attacks/Gradcam.py
@@ -8,10 +8,18 @@ class Gradcam(object):
         self.model = model
         self.layername = layername
         self.classindex =  index
+        if self.checklayername == None:
+            self.getlastlayer()
     
     def __str__(self):
         return "Grad cam implementation"
     
 
     def checklayername(self):
-        '''
+        '''check if layer is not none'''
+        return self.layername == None
+
+    def getlastlayer(self):
+        '''Finding the last layer'''
+
+
