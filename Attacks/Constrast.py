f027685f37f6270d26342f8f8a39228c6cec31c9
goodship1 <msgoodship1@gmail.com>
2021-03-13 01:05:19 +0000

    contrast.py fixing errors and model support

--- Attacks/Constrast.py
+++ Attacks/Constrast.py
@@ -1,15 +1,27 @@
 import cv2 as cv
+import numpy as np
 class Contrast(object):
     '''Class for adding constrast to image to reduce to image quality'''
     
     def __str__(self):
         return "functions for adding constrast"
-
-    def constrast(self,image,levelofconstrast):
-        alpha = constrast
+    
+    def returnpostive(self,contrast):
+        if constrast <0:
+            return abs(contrast)
+    
+    def constrast(self,image,levelofconstrast,model=None):
+        if levelofcontrast <0:
+            levelofcontrast = self.returnpostive(levelofcontrast)
+        alpha = levelofcontrast
         beta = 0
         convertimage = cv.imread(image)
         addconstrast = cv.convertScaleAbs(image,alpha = alpha, beta = beta)
-        return addconstrast
+        if model != None:
+            covnert_to_numpy = np.array(image)
+            pred =  model.predict(image)
+            return pred
+        elif model == None:
+            return addconstrast
 
 
