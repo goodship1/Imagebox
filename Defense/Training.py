630d9e4dd3b138a647880753503260e6c89f8637
Gavin Goodship <msgoodship1@gmail.com>
2021-03-23 21:31:50 +0000

    Update Training.py

--- Defense/Training.py
+++ Defense/Training.py
@@ -9,11 +9,11 @@ class Training(object):
         self.noise = Noise()
         self.contrast = Contrast()
     
-   def _gussianfile(image,filepath):
+   def _gussianfile(self,image,filepath):
        '''adds gussian noise images to file path'''
        pass
 
-   def _saltpepperfile(image,filepath):
+   def _saltpepperfile(self,image,filepath):
        pass
 
    def _rotationlefttrainingfile(image,filepath):
@@ -22,22 +22,40 @@ class Training(object):
    def _rotationrighttrainingfile(image,filepath):
        pass
 
-   def gussianTraining(image,filePath,numberofsamples):
+   def gussianTraining(self,image,filePath,numberofsamples,model,label):
        '''function for generating guassian noise
        images for training'''
+        samples_added = 0
         for x in range(numberofsamples):
-             gussian_image = self.noise.Gussian(image)
+             gussian_image = self.noise.Gussiannoise(image)
+             convert =  np.array(gussian_image)
+             if model.predict(convert) == label:
+                    self.gussianfile(image,file_path)
+                    samples_added +=1
+         return "number of samples added" + " " + str(samples_added) +" " + "to file path" +" " + filePath
+    
+                    
+             
   
   def randompixelchange(image,label,filepath):
       pass
 
-  def constrasttraining(image,filepath,numberofsamples):
+  def constrasttraining(image,filepath,numberofsamples,levelofcontrast)
       pass
 
-  def saltpeppertraining(image,filepath,numberofsamples):
-      speckle_noise =  self.noise.Speckle(image)
+  def saltpeppertraining(self,image,filepath,numberofsamples,model,label):
+        samples_added =  0
+        for x in numberofsamples:
+            speckle_noise =  self.noise.Speckle(image)
+            if model.predict(speckle_noise) == label:
+                self.speckle_filepath(image,filepath)
+                samples_added +=1
+        return "number of samples added" + " " + str(samples_added) +" " + "to file path" +" " + filePath
+        
+        
+            
 
- def rotationtrainingleft(image,filepath,radians):
+ def rotationtrainingleft(self,image,filepath,radians):
      '''rotates image'''
 
  def fgsmtraining(image,label,filepath,ep):
