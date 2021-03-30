097f1eadc2b7ec5b551f30730cea624a8197e7fc
goodship1 <msgoodship1@gmail.com>
2021-03-30 02:05:40 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-import setuptools
+from setuptools import setup
+
+setup(
+	  name = 'Imagebox',
+	  version = '1.0',
+	  description ='adv machine learning',
+	  author ='gavin goodship',
+	  packages=['Attacks','Defense'],
+	  install_requires=["keras","tensorflow","pytest","cv2","numpy"]
+	  )
