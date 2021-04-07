44a8c5b025f21210368a050970a4e5122b7a1e7a
goodship1 <msgoodship1@gmail.com>
2021-04-07 11:23:32 +0100

    adding blurtest

---
+++ Tests/Blurtests.py
@@ -0,0 +1,26 @@
+from Imagebox.Attacks import Blur
+class Blurtests(object):
+
+    def __init__(self):
+        self.image = None
+        self.model = None
+        self.label = None
+        self.kernel_size = (3,3)
+        self.percent = 5 
+
+    def gussainblurimage(self):
+        pass
+
+    def gussainblurmodel(self):
+        pass
+    
+    def averageblurimage(self):
+        pass
+    def averageblurmodel(self):
+        pass
+    def medianblurimage(self):
+        pass
+    def medianblurmode(self):
+        pass
+
+    
