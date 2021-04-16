af69502526c990b9ac375cdf77037c6b949d9e76
goodship1 <msgoodship1@gmail.com>
2021-04-16 21:43:32 +0100

    adding origin manin

--- Attacks/Blur.py
+++ Attacks/Blur.py
@@ -7,15 +7,13 @@ class Blur(object):
     def __str__(self):
         return "Applies various types of Blurs to iamges"
     
-	
-	def checkboxfilter(self,box):
-		if box[0] % 2!= 0 and box[1] !=0:
-			return True
-		else:
-			return False
-	
-	def checkpostive(self,percent):
-		return percent > 0
+    def checkboxfilter(self,box):
+      if box[0] % 2!= 0 and box[1] !=0:
+        return True
+      else:
+        return False
+    def checkpostive(self,percent):
+		    return percent > 0
 	
     def guassianblur(self,stdiv,image,kernel_size,model = None,label= None):
         '''Function takes an image
@@ -23,34 +21,37 @@ class Blur(object):
         stakes standard div and image path tuple for kernel size'''
         check =  self.checkboxfilter(kernel_size)
         if check == True:
-			image = cv.imread(image)
-			image =  cv.GaussianBlur(image,kernel_size,stdiv)
-			if model != None:
-					pred = np.argmax(model.predict(image.reshape(1,image.shape[0],image.shape[1],image.shape[2])))
-					return (pred,image,stdiv,kernel_size)
-			else:
-				return image
+			      image = cv.imread(image)
+			      image =  cv.GaussianBlur(image,kernel_size,stdiv)
+        if model != None:
+          pred = np.argmax(model.predict(image.reshape(1,image.shape[0],image.shape[1],image.shape[2])))
+          return (pred,image,stdiv,kernel_size)
+        else:
+          return image
 			
-    def averageblur(self,image,box = (3,3),model = None,label = None):
-	   image = cv.imread(image)
-	   if self.checkboxfilter(box):
-		   average_filter = cv.blur(image,(box))
-		   if model != None:
-			   pred = np.argmax(model.predict(average_filter.reshape(1,image.shape[0],image.shape[1],image.shape[2])))
-			   return (pred,average_filter,box)
-		   else:
-			   return average_filter
+    
+          
     
     
     def medianblur(self,image,percent,model=None):
-		if self.checkpostive(percent):
-				image = cv.imread(image)
-				median = cv.medianBlur(image,percent)
-				if model!= None:
-					pred = np.argmax(model.predict(median.reshape(1,median.shape[0],median.shape[1],median.shape[2])))
-					return (pred,median,percent)
-				else:
-					return median
+      if self.checkpostive(percent):
+            image = cv.imread(image)
+            median = cv.medianBlur(image,percent)
+      if model!= None:
+        pred = np.argmax(model.predict(median.reshape(1,median.shape[0],median.shape[1],median.shape[2])))
+        return (pred,median,percent)
+      else:
+        return median
+    
+    def averageblur(self,image,box = (3,3),model = None,label = None):
+      image = cv.imread(image)
+      if self.checkboxfilter(box):
+        average_filter = cv.blur(image,(box))
+      if model != None:
+        pred = np.argmax(model.predict(average_filter.reshape(1,image.shape[0],image.shape[1],image.shape[2])))
+        return (pred,average_filter,box)
+      else:
+        return average_filter
 	
 			
 		
